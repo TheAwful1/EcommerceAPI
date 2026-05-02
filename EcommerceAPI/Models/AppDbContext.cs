8ebae3e0ebfeb39e0e454a1eb37608ffe255e0e9
Edwin Felipe <105158452+TheAwful1@users.noreply.github.com>
2026-05-02 04:25:33 +0000

    A

--- EcommerceAPI/Models/AppDbContext.cs
+++ EcommerceAPI/Models/AppDbContext.cs
@@ -42,7 +42,7 @@ public partial class AppDbContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
        {
-    if (!optionsBuilder.IsConfigured)//Cuidado siempre con esto, en futuros proyectos. Cuidado.
+    if (!optionsBuilder.IsConfigured)//Cuidado siempre con esto, en futuros proyectos.
         optionsBuilder.UseSqlServer("server=CPADEFELIPE;database=Ecomerce;trusted_connection=true;TrustServerCertificate=True;");
 }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
