a2548221fc6542acd188e790aa06799988a720e1
Edwin Felipe <105158452+TheAwful1@users.noreply.github.com>
2026-05-23 05:54:17 +0000

    Cambios en el String de conexion, tambien cambie la base de datos para que sea MySql y no SQLServer.

--- EcommerceAPI/Program.cs
+++ EcommerceAPI/Program.cs
@@ -27,7 +27,9 @@ builder.Services.AddHttpContextAccessor();
 builder.Services.AddScoped<CartService>();
 
 
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),ServerVersion.AutoDetect(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    )));
 
 
 
